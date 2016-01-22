168b425e4e81528044c9f7847fe9ae9002570f96
RobinBKing <samimspop@gmail.com>
2016-01-22 13:51:04 -0800

    add form code, and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,11 @@ var numberCheck = function(countTo) {
 };
 var numberArray = function(countTo) {
   var countArray = [];
-  for (var i = 1; i < (countTo + 1); i++){
-    countArray.push(i);
-  };
+  if(numberCheck(countTo)){
+    for (var i = 1; i < (countTo + 1); i++){
+      countArray.push(i);
+    };
+  }
   return countArray;
 };
 var remainderCheck = function(divTo, divBy) {
@@ -14,39 +16,40 @@ var remainderCheck = function(divTo, divBy) {
 };
 var numberReplace = function(countArray, replaceNbr, replaceString) {
   for (var i = 0; i < (countArray.length); i++){
-    if(countArray[i] % replaceNbr ===0){
+    if(remainderCheck(countArray[i], replaceNbr)){
       countArray[i] = replaceString;
     }
   };
   return countArray;
 };
 var pingPongReplace = function(countArray) {
-  if(countArray.length > 14){
-    countArray = numberReplace(countArray, 15, "PingPong");
+  if(countArray.length > 0){
+    countArray = numberReplace(countArray, 15, "Ping Pong");
     countArray = numberReplace(countArray, 5, "Pong");
     countArray = numberReplace(countArray, 3, "Ping");
   }
   return countArray;
 };
 var makePingPong = function(countTo) {
-  var countArray;
-  var pingPongArray;
-  if(numberCheck(countTo)){
-    countArray = numberArray(countTo);
-    pingPongArray = pingPongReplace(countArray);
-  };
+  var countArray = numberArray(countTo);
+  var pingPongArray = pingPongReplace(countArray);
   return pingPongArray;
 };
 //interface
 $(document).ready(function() {
-  // $("form#pingPong").submit(function(event) {
-  // $("ul").empty();
-  // var countTo = parseInt($("input#countTo").val());
-  //
-  // var listArray = pingPongReplace(countTo, countBy);
-  // for (var i = 0; i < listArray.length; i++){
-  //   $("ul.pingPongList").append("<li>"+listArray[i]+"</li>");
-  // }
-  // $("#result").show();
-  // event.preventDefault();
+  $("form#pingPong").submit(function(event) {
+  $("ul").empty();
+  var countTo = parseInt($("input#countTo").val());
+  var listArray = makePingPong(countTo);
+  if(listArray.length > 0){
+    for (var i = 0; i < listArray.length; i++){
+      $("ul.pingPongList").append("<li>"+listArray[i]+"</li>");
+    }
+    $("#result").show();
+  }
+  else {
+    alert("You have to enter a number to play!");
+  }
+  event.preventDefault();
+  });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,4 @@
-describe('pingPong', function() {
+describe('Ping Pong', function() {
   it("is true if countTo is a number", function(){
     expect(numberCheck(50)).to.equal(true);
   });
@@ -8,7 +8,7 @@ describe('pingPong', function() {
   it("is true if countTo is > 0", function(){
     expect(numberCheck(1)).to.equal(true);
   });
-  it("is true if remaindir array number divided by dcountTo = 0", function(){
+  it("is true if remainder array number divided by countTo = 0", function(){
     expect(numberCheck(1)).to.equal(true);
   });
   it("is true if 1st number divided by 2nd number's remaindier = 0", function(){
@@ -22,9 +22,9 @@ describe('pingPong', function() {
     var countArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
     expect(numberReplace(countArray, 5, "Pong")).to.eql([1,2,3,4,"Pong",6,7,8,9,"Pong",11,12,13,14,"Pong",16,17,18,19,"Pong",21,22,23,24,"Pong",26,27,28,29,"Pong"]);
   });
-  it("should replace array number divisible by 15 with 'PingPong'", function() {
+  it("should replace array number divisible by 15 with 'Ping Pong'", function() {
       var countArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-      expect(numberReplace(countArray, 15, "PingPong")).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,"PingPong",16,17,18,19,20,21,22,23,24,25,26,27,28,29,"PingPong"]);
+      expect(numberReplace(countArray, 15, "Ping Pong")).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,"Ping Pong",16,17,18,19,20,21,22,23,24,25,26,27,28,29,"Ping Pong"]);
   });
   it("should replace array number divisible by any given with given text", function() {
       var countArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
@@ -32,9 +32,9 @@ describe('pingPong', function() {
   });
   it("should replace array numbers divisible by given numbers with given text", function() {
       var countArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-      expect(pingPongReplace(countArray)).to.eql([1, 2, "Ping", 4, "Pong", "Ping", 7, 8, "Ping", "Pong", 11, "Ping", 13, 14, "PingPong", 16, 17, "Ping", 19, "Pong", "Ping", 22, 23, "Ping", "Pong", 26, "Ping", 28, 29, "PingPong"]);
+      expect(pingPongReplace(countArray)).to.eql([1, 2, "Ping", 4, "Pong", "Ping", 7, 8, "Ping", "Pong", 11, "Ping", 13, 14, "Ping Pong", 16, 17, "Ping", 19, "Pong", "Ping", 22, 23, "Ping", "Pong", 26, "Ping", 28, 29, "Ping Pong"]);
   });
-  it("should a PingPong array", function() {
-      expect(makePingPong(30)).to.eql([1, 2, "Ping", 4, "Pong", "Ping", 7, 8, "Ping", "Pong", 11, "Ping", 13, 14, "PingPong", 16, 17, "Ping", 19, "Pong", "Ping", 22, 23, "Ping", "Pong", 26, "Ping", 28, 29, "PingPong"]);
+  it("should a return a PingPong array", function() {
+      expect(makePingPong(30)).to.eql([1, 2, "Ping", 4, "Pong", "Ping", 7, 8, "Ping", "Pong", 11, "Ping", 13, 14, "Ping Pong", 16, 17, "Ping", 19, "Pong", "Ping", 22, 23, "Ping", "Pong", 26, "Ping", 28, 29, "Ping Pong"]);
   });
 });
