3c5704bac5021a8d8fc7fed53f3ddb403cb80a7d
RobinBKing <samimspop@gmail.com>
2016-01-22 09:28:56 -0800

    add sussesful test for remainder

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 //business logic
-var numberCheck = function(countToNumber) {
-  return !isNaN(countToNumber);
+var numberCheck = function(numberTo) {
+  return !isNaN(numberTo);
 };
-
-
-
+var numberArray = function(numberTo) {
+  var countArray = [];
+  for (var i =1; i < (numberTo + 1); i++){
+    countArray.push(i);
+  };
+  return countArray;
+};
+var remainderCheck = function(divTo, divBy) {
+  return (divTo % divBy === 0);
+};
+// var numberReplace function(arrayNbr, replaceNbr, replaceString) {
+//   return true;
+// }
 
 //interface
 // $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,20 @@
 describe('pingPong', function() {
-  it("is true if countToNumber is a number", function(){
+  it("is true if countTo is a number", function(){
     expect(numberCheck(50)).to.equal(true);
   });
-  it("is false if countToNumber is not a number", function(){
+  it("is false if countTo is not a number", function(){
     expect(numberCheck("not")).to.equal(false);
   });
+  it("is true if countTo is > 0", function(){
+    expect(numberCheck(1)).to.equal(true);
+  });
+  it("is true if remaindir array number divided by dcountTo = 0", function(){
+    expect(numberCheck(1)).to.equal(true);
+  });
+  it("is true if 1st number divided by 2nd number's remaindier = 0", function(){
+    expect(remainderCheck(30, 15)).to.equal(true);
+  });
+  //  it("should replace array number divisible by given number with given text", function() {
+  //     expect(numberReplace(30, 15, "PingPong")).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,"PingPong",16,17,18,19,20,21,22,23,24,25,26,27,28,29,"PingPong"]);
+  //   });
 });
